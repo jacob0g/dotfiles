2ddcee292fc57b75c6147474482254c70eedbc23
jacob0g <gallop.jacob@gmail.com>
2026-06-22 18:39:42 +0800

    updated treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
         branch = "main",
         lazy = false,
         build = ":TSUpdate",
